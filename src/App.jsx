e30d7c3445126ba50cab648e17b38ea4e3aae9f1
Dashersd <helmandacuma5@gmail.com>
2025-12-26 19:33:06 +0800

    Restore original static background with radial gradients, remove animated grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
-import AnimatedBackground from './components/AnimatedBackground'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen relative">
-        <AnimatedBackground />
-        <div className="relative z-10">
-          <Header />
-          <Home />
-        </div>
+      <div className="min-h-screen bg-gray-900">
+        <Header />
+        <Home />
       </div>
     </Router>
   )
